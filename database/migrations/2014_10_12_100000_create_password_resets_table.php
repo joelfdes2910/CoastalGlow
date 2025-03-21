d6c117ca83f10d90736afe2ca4cbfbf8b1c6f80d
muhammad.shariq <muhammad.shariq@informiatech.com>
2025-03-21 21:49:30 +0500

    - fix password reset migration

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->engine = 'InnoDB';
+            $table->string('email', 255)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+            $table->charset = 'utf8'; // Ensure the character set is utf8
         });
     }
 
