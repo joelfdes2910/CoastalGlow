63d1d23fa52ad7352843be8f7db719b8c3da9f22
joelfdes2910 <Joelfernandes04@gmail.com>
2025-04-08 11:26:25 -0400

    change company name

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="main-footer text-center">
-    <strong>&copy; {{ date('Y') }} Your Company Name.</strong> All rights reserved.
+    <strong>&copy; {{ date('Y') }} Coastal Glow.</strong> All rights reserved.
 </footer>
